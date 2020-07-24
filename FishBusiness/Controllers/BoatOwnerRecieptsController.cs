704368a630f6c1d1dfe556593ce4f66f971c003a
Ehab Ibrahim <engehab245@gmail.com>
2020-07-24 22:05:09 +0200

    Adding Boat Owner Receipt

--- FishBusiness/Controllers/BoatOwnerRecieptsController.cs
+++ FishBusiness/Controllers/BoatOwnerRecieptsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using FishBusiness;
 using FishBusiness.Models;
+using FishBusiness.ViewModels;
 
 namespace FishBusiness.Controllers
 {
@@ -49,8 +50,11 @@ namespace FishBusiness.Controllers
         // GET: BoatOwnerReciepts/Create
         public IActionResult Create()
         {
-            ViewData["BoatID"] = new SelectList(_context.Boats, "BoatID", "BoatLeader");
-            ViewData["SarhaID"] = new SelectList(_context.Sarhas, "SarhaID", "SarhaID");
+            ViewData["BoatID"] = new SelectList(_context.Boats, "BoatID", "BoatName");
+            ViewData["ProductionTypeID"] = new SelectList(_context.ProductionTypes, "ProductionTypeID", "ProductionName");
+            ViewData["FishID"] = new SelectList(_context.Fishes, "FishID", "FishName");
+            // commission
+            ViewBag.Commission = _context.Cofigs.Find(1);
             return View();
         }
 
@@ -59,17 +63,70 @@ namespace FishBusiness.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("BoatOwnerRecieptID,BoatID,SarhaID,TotalBeforePaying,Date,Commission,PaidFromDebts,TotalAfterPaying")] BoatOwnerReciept boatOwnerReciept)
+        public async Task<IActionResult> Create(BoatOwnerReciept boatOwnerReciept)
         {
-            if (ModelState.IsValid)
+           // Cookies Of Boat Owner Receipt
+            var TotalBeforePaymentCookie = Request.Cookies["TotalBeforePayment"];
+            var commisionCookie = Request.Cookies["commision"];
+            var PaidFromDebtsCookie = Request.Cookies["PaidFromDebts"];
+            var TotalProductionCookie = Request.Cookies["TotalProduction"];
+            //find latest sarha related to selected boat
+            var sarhaId = _context.Sarhas.Where(x => x.BoatID == boatOwnerReciept.BoatID).Max(x=>x.SarhaID);
+            boatOwnerReciept.SarhaID = sarhaId;
+            boatOwnerReciept.TotalBeforePaying = Convert.ToDecimal(TotalBeforePaymentCookie);
+            boatOwnerReciept.Commission = Convert.ToDecimal(commisionCookie);
+            boatOwnerReciept.PaidFromDebts = Convert.ToDecimal(PaidFromDebtsCookie);
+            boatOwnerReciept.TotalAfterPaying = Convert.ToDecimal(TotalProductionCookie);
+            // Subtracting Paid From Halek
+            var boat = _context.Boats.Find(boatOwnerReciept.BoatID);
+            boat.DebtsOfHalek -= Convert.ToDecimal(PaidFromDebtsCookie);
+            // Salary for Each One
+            var sarha = _context.Sarhas.Find(sarhaId);
+            var IndividualSalary = (Convert.ToDecimal(TotalProductionCookie) / 2) / sarha.NumberOfFishermen;
+            // Calculating Final Income 
+            // for shared boats
+            decimal FinalIncome;
+            // 5 -> Shared Boat ... We will change it later
+            if (boat.TypeID == 5)
             {
-                _context.Add(boatOwnerReciept);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                FinalIncome = (Convert.ToDecimal(TotalProductionCookie) / 2) - IndividualSalary;
+                boat.IncomeOfSharedBoat += FinalIncome;
             }
-            ViewData["BoatID"] = new SelectList(_context.Boats, "BoatID", "BoatLeader", boatOwnerReciept.BoatID);
-            ViewData["SarhaID"] = new SelectList(_context.Sarhas, "SarhaID", "SarhaID", boatOwnerReciept.SarhaID);
-            return View(boatOwnerReciept);
+            // for ordinary boats
+            else
+                FinalIncome = Convert.ToDecimal(TotalProductionCookie);
+            boatOwnerReciept.FinalIncome = FinalIncome;
+            _context.Add(boatOwnerReciept);
+            await _context.SaveChangesAsync();
+            // Cookies Of Receipt Items
+            var FishesCookie = Request.Cookies["FishNames"];
+            var ProductionTypesCookie = Request.Cookies["ProductionTypes"];
+            var qtysCookie = Request.Cookies["qtys"];
+            var unitpricesCookie = Request.Cookies["unitprices"];
+            var pricesCookie = Request.Cookies["prices"];
+            string[] Fishes = FishesCookie.Split(",").Select(c => Convert.ToString(c)).ToArray();
+            string[] Productions = ProductionTypesCookie.Split(",").Select(c => Convert.ToString(c)).ToArray();
+            int[] qtys = qtysCookie.Split(",").Select(c => Convert.ToInt32(c)).ToArray();
+            decimal[] unitPrices = unitpricesCookie.Split(",").Select(c => Convert.ToDecimal(c)).ToArray();
+            decimal[] prices = pricesCookie.Split(",").Select(c => Convert.ToDecimal(c)).ToArray();
+            var latestReceipt = _context.BoatOwnerReciepts.Max(x => x.BoatOwnerRecieptID);
+            for (int i = 0; i < Fishes.Length; i++)
+            {
+                var fish = _context.Fishes.Single(x => x.FishName == Fishes[i]);
+                var Produc = _context.ProductionTypes.Single(x => x.ProductionName == Productions[i]);
+                BoatOwnerItem boatOwnerItem = new BoatOwnerItem()
+                {
+                    BoatOwnerRecieptID = latestReceipt,
+                    FishID = fish.FishID,
+                    ProductionTypeID = Produc.ProductionTypeID,
+                    Qty = qtys[i],
+                    UnitPrice = unitPrices[i],
+                };
+                _context.BoatOwnerItems.Add(boatOwnerItem);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+           
         }
 
         // GET: BoatOwnerReciepts/Edit/5
---
+++ FishBusiness/ViewModels/BoatReceiptViewModel.cs
@@ -0,0 +1,14 @@
+﻿using FishBusiness.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FishBusiness.ViewModels
+{
+    public class BoatReceiptViewModel
+    {
+        public BoatOwnerReciept BoatOwnerReciept  { get; set; }
+        public BoatOwnerItem BoatOwnerItem  { get; set; }
+    }
+}

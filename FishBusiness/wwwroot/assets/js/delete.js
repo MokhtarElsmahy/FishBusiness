bdf03ffc94e1e62f7501c9303ac5f19ae953351f
Ehab Ibrahim <engehab245@gmail.com>
2020-07-29 16:16:07 +0200

    Adding SweetAlert to Confirm Deletion in all Datatables & Saving Receipts

---
+++ FishBusiness/wwwroot/assets/js/delete.js
@@ -0,0 +1,72 @@
+﻿$((function () {
+    var url;
+    var redirectUrl;
+    var target;
+
+    $('body').append(`
+            <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">عملية حذف</h4>
+                </div>
+                <div class="modal-body delete-modal-body">
+                    
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal" id="cancel-delete">الغاء</button>
+                    <button type="button" class="btn btn-danger" id="confirm-delete">حذف</button>
+                </div>
+                </div>
+            </div>
+            </div>`);
+
+    //Delete Action
+    $(".delete").on('click', (e) => {
+        e.preventDefault();
+
+        target = e.target;
+        var Id = $(target).data('id');
+        var controller = $(target).data('controller');
+        var action = $(target).data('action');
+        var bodyMessage = $(target).data('body-message');
+        redirectUrl = $(target).data('redirect-url');
+
+        url = "/" + controller + "/" + action + "?Id=" + Id;
+        $(".delete-modal-body").text(bodyMessage);
+        $("#deleteModal").modal('show');
+    });
+
+    $("#confirm-delete").on('click', () => {
+        $.get(url)
+            .done((result) => {
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: 'تم الحذف بنجاح',
+                    showConfirmButton: false,
+                    timer: 3000
+                });
+
+                if (!redirectUrl) {
+                    return $(target).parent().parent().hide("slow");
+                }
+                window.location.href = redirectUrl;
+            })
+            .fail((error) => {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'حدث خطأ',
+                    text: 'حاول مرة اخري',
+                });
+
+
+                if (redirectUrl)
+                    window.location.href = redirectUrl;
+            }).always(() => {
+                $("#deleteModal").modal('hide');
+            });
+    });
+
+}()));

60d05e3d039c52df2a418edb389d15a4e667ecf3
Ehab Ibrahim <engehab245@gmail.com>
2020-10-09 00:03:44 +0200

    Distribution

--- FishBusiness/ViewModels/MerRecCreateVm.cs
+++ FishBusiness/ViewModels/MerRecCreateVm.cs
@@ -7,30 +7,18 @@ namespace FishBusiness.ViewModels
 {
     public class MerRecCreateVm
     {
-        public DateTime Date { get; set; }
-
-        public int MerchantID { get; set; }
-
-
-
-        public decimal TotalOfReciept { get; set; }
-
         public int RecID { get; set; }
-
-        public decimal payment { get; set; }
-        public decimal CurrentDebt { get; set; }
-
+        public DateTime Date { get; set; }
+        public int MerchantID { get; set; }
         public string FishNames { get; set; }
+        public string MerchantNames { get; set; }
+        public string boats { get; set; }
         public string ProductionTypes { get; set; }
         public string qtys { get; set; }
         public string unitprices { get; set; }
-        public string boats { get; set; }
-       
+        public decimal payment { get; set; }
+        public decimal TotalOfReciept { get; set; }
+        public decimal CurrentDebt { get; set; }
 
-        //FishNames: FishNames,
-        //                ProductionTypes: ProductionTypes,
-        //                qtys: qtys,
-        //                unitprices: unitprices,
-        //                boats: boats
     }
 }
